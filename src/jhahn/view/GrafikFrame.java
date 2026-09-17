b0618785f5c7565ae5145440c077f4c32cec7058
Falcon <jason.hahn9@gmail.com>
2026-09-17 22:55:41 +0200

    grund Struktur von dem Frame erstellt

--- src/jhahn/view/GrafikFrame.java
+++ src/jhahn/view/GrafikFrame.java
@@ -1,4 +1,18 @@
 package jhahn.view;
 
-public class GrafikFrame {
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class GrafikFrame extends JFrame {
+    private final GrafikPanel panel;
+    public GrafikFrame(ActionListener controller) {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(400, 400);
+        panel = new GrafikPanel();
+        this.add(panel);
+    }
+    public GrafikPanel getPanel() {
+        return panel;
+    }
 }

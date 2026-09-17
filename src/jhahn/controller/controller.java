f4bec690d5b8baa61ee6dff2a3702e5b5ad9053a
Falcon <jason.hahn9@gmail.com>
2026-09-17 23:37:39 +0200

    buntere Oberfläche: wenn das rundenErgebnis -5 oder man verloren hat, werden die Hintergründe der Label rot. Wenn es Positiv ist oder man gewonnen hat, werden die Hintergründe der Label grün. Wenn eine neue Runde gespielt wird werden sie wieder weiß

--- src/jhahn/controller/controller.java
+++ src/jhahn/controller/controller.java
@@ -29,15 +29,32 @@ public class controller implements ActionListener {
             }
             model.berechneRunde(eingabe);
             JLabel rundenErgebnis = panel.getRundenErgebnis();
-            if (model.hatGewonnen()) rundenErgebnis.setText("Gewonnen");
-            else if (model.hatVerloren()) rundenErgebnis.setText("Verloren");
-            else rundenErgebnis.setText(Integer.toString(model.getRundenErgebnis()));
+            if (model.hatGewonnen()) {
+                rundenErgebnis.setText("Gewonnen");
+                rundenErgebnis.setBackground(Color.GREEN);
+            }
+            else if (model.hatVerloren()) {
+                rundenErgebnis.setText("Verloren");
+                rundenErgebnis.setBackground(Color.RED);
+            }
+            else {
+                rundenErgebnis.setText(Integer.toString(model.getRundenErgebnis()));
+                if (model.getRundenErgebnis() > 0) {
+                    rundenErgebnis.setBackground(Color.GREEN);
+                    panel.getGesamtPunkte().setBackground(Color.GREEN);
+                } else if (model.getRundenErgebnis() < 0) {
+                    rundenErgebnis.setBackground(Color.RED);
+                    panel.getGesamtPunkte().setBackground(Color.RED);
+                }
+            }
             panel.getComputerPunkte().setText(Integer.toString(model.getComputerZahl()));
             panel.getGesamtPunkte().setText(Integer.toString(model.getGesamtPunkte()));
         } else if (e.getSource() == panel.getNochMalBtn()) {
             panel.getComputerPunkte().setText("");
             panel.getRundenErgebnis().setText("");
             panel.getEingabe().setText("");
+            panel.getRundenErgebnis().setBackground(Color.WHITE);
+            panel.getGesamtPunkte().setBackground(Color.WHITE);
         }
     }
 

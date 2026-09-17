8b1874bc3863f8bfdda487b07e497ce9667e5bd5
Falcon <jason.hahn9@gmail.com>
2026-09-17 22:43:43 +0200

    Model Struktur aufgebaut

--- src/jhahn/model/GewinnModel.java
+++ src/jhahn/model/GewinnModel.java
@@ -1,4 +1,34 @@
 package jhahn.model;
 
 public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel() {
+
+    }
+    public void berechneComputerZahl() {
+
+    }
+    public void berechneRunde(int spielerZahl) {
+
+    }
+    public boolean hatGewonnen() {
+
+    }
+    public boolean hatVerloren() {
+
+    }
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
+
 }

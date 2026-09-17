3dcffdbc4f1e229f393c2c2c0e7036836f78c98d
Falcon <jason.hahn9@gmail.com>
2026-09-17 22:47:50 +0200

    unkomplizierte restliche model codes (randomzahl, returns, initialwerte)

--- src/jhahn/model/GewinnModel.java
+++ src/jhahn/model/GewinnModel.java
@@ -1,5 +1,7 @@
 package jhahn.model;
 
+import java.util.Random;
+
 public class GewinnModel {
     private int gesamtPunkte;
     private int spielerZahl;
@@ -7,19 +9,19 @@ public class GewinnModel {
     private int rundenErgebnis;
 
     public GewinnModel() {
-
+        gesamtPunkte = 30;
     }
     public void berechneComputerZahl() {
-
+        computerZahl = new Random().nextInt(1,10);
     }
     public void berechneRunde(int spielerZahl) {
 
     }
     public boolean hatGewonnen() {
-
+        return gesamtPunkte >= 100;
     }
     public boolean hatVerloren() {
-
+        return gesamtPunkte <= 0;
     }
     public int getGesamtPunkte() {
         return gesamtPunkte;
